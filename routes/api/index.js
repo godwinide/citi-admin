82483ff0ac98369c285f1c0bf30fd1f63e5b0b4e
Godwin Idemudia <godwinide@gmail.com>
2022-02-27 23:50:36 +0100

    new

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../../model/User");
 
 router.post("/login", async (req,res) => {
+    console.log(req.body)
     try{
         const {email, password} = req.body;
         if(!email || !password){
@@ -20,6 +21,51 @@ router.post("/login", async (req,res) => {
     }catch(err){
         return res.json({success: false, msg:"Something went wrong"});
     }
-})
+});
+
+
+router.post("/change-password", async(req,res) => {
+    try{
+        const {password, password2, id} = req.body
+        if(!password || !password2){
+            return res.json({
+                success: false,
+                msg: "Fill all required fields!"
+            });
+        }
+        if(password !== password2){
+            return res.json({
+                success: false,
+                msg: "Both passwords are not thesame"
+            });
+        }
+        if(password.length < 6){
+            return res.json({
+                success: false,
+                msg: "password is too short"
+            })
+        }
+        const user = await User.findById(id);
+        if(!user){
+            return res.json({
+                success: false,
+                msg: "Invalid credentials"
+            });
+        }
+        await User.updateOne({_id:id}, {
+            password
+        });
+        return res.json({
+            success: true,
+            msg: "Password changed successfully"
+        });
+    }catch(err){
+        console.log(err);
+        return res.json({
+            success: false,
+            msg: "internal server error"
+        })
+    }
+});
 
 module.exports = router;
